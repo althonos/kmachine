d9d9bbd37f85ccde719412e101174515594c4ec3
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-02-25 13:07:06 +0100

    Add `Display` implementation for `::kappa::Agent` and `::kappa::Link`

--- src/kappa/agent.rs
+++ src/kappa/agent.rs
@@ -1,7 +1,9 @@
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::fmt::Result as FmtResult;
+use std::fmt::Write;
 
 use indexmap::IndexSet;
-
-
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Agent {
@@ -23,11 +25,28 @@ impl Agent {
     }
 }
 
+impl Display for Agent {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}(", self.name)?;
+        let sites = &mut self.sites.iter().peekable();
+        while let Some(ref site) = sites.next() {
+            if sites.peek().is_some() {
+                write!(f, "{}, ", site)?;
+            } else {
+                write!(f, "{}", site)?;
+            }
+        }
+        f.write_str(")")
+    }
+}
+
+
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Site {
     name: String,
     states: IndexSet<String>,
-    bindings: IndexSet<Stub>,
+    links: IndexSet<Link>,
 }
 
 impl Site {
@@ -35,16 +54,21 @@ impl Site {
         Self {
             name: name.into(),
             states: IndexSet::new(),
-            bindings: IndexSet::new(),
+            links: IndexSet::new(),
         }
     }
 
-    pub fn binding<A, S>(&mut self, agent: A, site: S) -> &mut Self
+    pub fn link(&mut self, link: Link) -> &mut Self {
+        self.links.insert(link);
+        self
+    }
+
+    pub fn linkable<A, S>(&mut self, agent: A, site: S) -> &mut Self
     where
         A: Into<String>,
         S: Into<String>,
     {
-        self.bindings.insert(Stub::new(agent, site));
+        self.links.insert(Link::BoundTo { agent: agent.into(), site: site.into()});
         self
     }
 
@@ -57,21 +81,58 @@ impl Site {
     }
 }
 
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
-struct Stub {
-    agent: String,
-    site: String,
+impl Display for Site {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        f.write_str(&self.name)?;
+        if !self.states.is_empty() {
+            let states = &mut self.states.iter().peekable();
+            f.write_char('{')?;
+            while let Some(ref state) = states.next() {
+                if states.peek().is_some() {
+                    write!(f, "{}, ", state)?;
+                } else {
+                    write!(f, "{}", state)?;
+                }
+            }
+            f.write_char('}')?;
+        }
+        if !self.links.is_empty() {
+            let links = &mut self.links.iter().peekable();
+            f.write_char('[')?;
+            while let Some(ref link) = links.next() {
+                if links.peek().is_some() {
+                    write!(f, "{}, ", link)?;
+                } else {
+                    write!(f, "{}", link)?;
+                }
+            }
+            f.write_char(']')?;
+        }
+        Ok(())
+    }
 }
 
-impl Stub {
-    pub fn new<A, S>(agent: A, site: S) -> Self
-    where
-        A: Into<String>,
-        S: Into<String>,
-    {
-        Self {
-            agent: agent.into(),
-            site: site.into(),
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum Link{
+    Unknown,
+    Free,
+    Numbered(usize),
+    Bound,
+    BoundTo {
+        agent: String,
+        site: String,
+    }
+}
+
+impl Display for Link {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        match self {
+            Link::Unknown => f.write_char('#'),
+            Link::Free => f.write_char('.'),
+            Link::Numbered(n) => n.fmt(f),
+            Link::Bound => f.write_char('_'),
+            Link::BoundTo { site, agent } => write!(f, "{}.{}", site, agent),
         }
     }
 }
