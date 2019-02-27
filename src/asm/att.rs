2f42b5cdeb1c5dcbac78455bdd5773c27720c051
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-02-27 03:54:34 +0100

    Rewrite `::asm` to use borrowed strings

--- src/asm/att.rs
+++ src/asm/att.rs
@@ -5,6 +5,7 @@ use std::collections::VecDeque;
 use ::pest::Parser;
 
 use super::AsmParser;
+use super::AsmProgram;
 use super::Label;
 use super::Line;
 use super::Op;
@@ -15,56 +16,52 @@ use super::Register;
 pub struct AttParser;
 
 impl AsmParser for AttParser {
-    fn parse_asm(s: &str) -> Vec<Line> {
-        let pairs = AttParser::parse(Rule::program, s).unwrap_or_else(|e| panic!("{}", e));
-
-        let mut program = Vec::new();
-
-        // Because ident_list is silent, the iterator will contain idents
-        for pair in pairs {
-            match pair.as_rule() {
-                Rule::label => {
-                    let ident = pair.into_inner().next().unwrap().as_str();
-                    program.push(Line::from(Label::new(ident.to_string())));
-                }
-
-                Rule::instruction => {
-                    let mut inner = pair.into_inner();
-                    let opname = inner.next().unwrap().as_str();
-
-                    let mut args: VecDeque<_> = inner.map(|r| r.into_inner()).collect();
-
-                    let op = match opname {
-                        "inc" => {
-                            let register = args.pop_front().unwrap().as_str();
-                            Op::Inc(Register::new(register.to_string()))
+    fn parse_asm<'a>(s: &'a str) -> AsmProgram<'a> {
+        AttParser::parse(Rule::program, s)
+            .map(|pairs| {
+                pairs
+                    .into_iter()
+                    .map(|pair| match pair.as_rule() {
+                        Rule::label => {
+                            let ident = pair.into_inner().next().unwrap().as_str();
+                            Line::from(Label::new(ident.to_string()))
                         }
-                        "dec" => {
-                            let register = args.pop_front().unwrap().as_str();
-                            Op::Dec(Register::new(register.to_string()))
+                        Rule::instruction => {
+                            let mut inner = pair.into_inner();
+                            let opname = inner.next().unwrap().as_str();
+
+                            let mut args: VecDeque<_> = inner.map(|r| r.into_inner()).collect();
+
+                            let op = match opname {
+                                "inc" => {
+                                    let register = args.pop_front().unwrap().as_str();
+                                    Op::Inc(Register::new(register.to_string()))
+                                }
+                                "dec" => {
+                                    let register = args.pop_front().unwrap().as_str();
+                                    Op::Dec(Register::new(register.to_string()))
+                                }
+                                "jz" => {
+                                    let register = args.pop_front().unwrap().as_str();
+                                    let label = args.pop_front().unwrap().as_str();
+                                    Op::Jz(
+                                        Register::new(register.to_string()),
+                                        Label::new(label.to_string()),
+                                    )
+                                }
+                                _ => unimplemented!(),
+                            };
+
+                            if !args.is_empty() {
+                                panic!("unused arguments for instruction `{:?}`: {:?}", op, args);
+                            }
+
+                            Line::from(op)
                         }
-                        "jz" => {
-                            let register = args.pop_front().unwrap().as_str();
-                            let label = args.pop_front().unwrap().as_str();
-                            Op::Jz(
-                                Register::new(register.to_string()),
-                                Label::new(label.to_string()),
-                            )
-                        }
-                        _ => unimplemented!(),
-                    };
-
-                    if !args.is_empty() {
-                        panic!("unused arguments for instruction `{:?}`: {:?}", op, args);
-                    }
-
-                    program.push(Line::from(op));
-                }
-
-                _ => unreachable!(),
-            }
-        }
-
-        program
+                        _ => unreachable!(),
+                    })
+                    .collect()
+            })
+            .unwrap_or_else(|e| panic!("{}", e))
     }
 }
--- src/asm/mod.rs
+++ src/asm/mod.rs
@@ -1,52 +1,94 @@
+//! Abstract syntax structures for counter machine programs.
+
 pub mod att;
 
+use std::borrow::Cow;
+use std::borrow::ToOwned;
+use std::iter::FromIterator;
+
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub struct Register {
-    pub name: String,
+pub struct Register<'a> {
+    pub name: Cow<'a, str>,
 }
 
-impl Register {
-    pub fn new(name: String) -> Self {
-        Self { name }
+impl<'a> Register<'a> {
+    pub fn new<I>(name: I) -> Self
+    where
+        I: Into<Cow<'a, str>>,
+    {
+        Self { name: name.into() }
     }
 }
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub struct Label {
-    pub name: String,
+pub struct Label<'a> {
+    pub name: Cow<'a, str>,
 }
 
-impl Label {
-    pub fn new(name: String) -> Self {
-        Self { name }
+impl<'a> Label<'a> {
+    pub fn new<I>(name: I) -> Self
+    where
+        I: Into<Cow<'a, str>>,
+    {
+        Self { name: name.into() }
     }
 }
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub enum Op {
-    Inc(Register),
-    Dec(Register),
-    Jz(Register, Label),
+pub enum Op<'a> {
+    Inc(Register<'a>),
+    Dec(Register<'a>),
+    Jz(Register<'a>, Label<'a>),
 }
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub enum Line {
-    LabelLine(Label),
-    OpLine(Op),
+pub enum Line<'a> {
+    LabelLine(Label<'a>),
+    OpLine(Op<'a>),
 }
 
-impl From<Label> for Line {
-    fn from(l: Label) -> Self {
+impl<'a> From<Label<'a>> for Line<'a> {
+    fn from(l: Label<'a>) -> Self {
         Line::LabelLine(l)
     }
 }
 
-impl From<Op> for Line {
-    fn from(op: Op) -> Self {
+impl<'a> From<Op<'a>> for Line<'a> {
+    fn from(op: Op<'a>) -> Self {
         Line::OpLine(op)
     }
 }
 
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct AsmProgram<'a> {
+    lines: Vec<Line<'a>>,
+}
+
+impl<'a> AsmProgram<'a> {
+    pub fn new() -> Self {
+        Self { lines: Vec::new() }
+    }
+
+    pub fn lines(&self) -> &Vec<Line<'a>> {
+        &self.lines
+    }
+
+    pub fn lines_mut(&mut self) -> &mut Vec<Line<'a>> {
+        &mut self.lines
+    }
+}
+
+impl<'a> FromIterator<Line<'a>> for AsmProgram<'a> {
+    fn from_iter<T>(iter: T) -> Self
+    where
+        T: IntoIterator<Item = Line<'a>>,
+    {
+        Self {
+            lines: iter.into_iter().collect(),
+        }
+    }
+}
+
 pub trait AsmParser {
-    fn parse_asm(s: &str) -> Vec<Line>;
+    fn parse_asm<'a>(s: &'a str) -> AsmProgram<'a>;
 }
