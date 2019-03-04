5d8ef9be8a2d335201a300d20077fa4832f4785d
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-03-04 13:20:14 +0100

    Implement `IntoIter` for `attasm::ast::Program`

--- attasm/src/ast/program.rs
+++ attasm/src/ast/program.rs
@@ -3,6 +3,8 @@ use std::fmt::Formatter;
 use std::fmt::Display;
 use std::fmt::Result as FmtResult;
 use std::iter::FromIterator;
+use std::vec::IntoIter as VecIntoIter;
+use std::slice::Iter as SliceIter;
 
 use super::Line;
 
@@ -13,11 +15,9 @@ pub struct Program<'a> {
 }
 
 impl<'a> Program<'a> {
-
     pub fn lines(&self) -> &Vec<Line<'a>> {
         &self.lines
     }
-
 }
 
 impl<'a> From<Vec<Line<'a>>> for Program<'a> {
@@ -36,6 +36,22 @@ impl<'a> FromIterator<Line<'a>> for Program<'a> {
         Self {
             lines: iter.into_iter().collect(),
         }
+    }
+}
+
+impl<'a> IntoIterator for Program<'a> {
+    type Item = Line<'a>;
+    type IntoIter = VecIntoIter<Line<'a>>;
+    fn into_iter(self) -> <Self as std::iter::IntoIterator>::IntoIter {
+        self.lines.into_iter()
+    }
+}
+
+impl<'a, 'b> IntoIterator for &'b Program<'a> {
+    type Item = &'b Line<'a>;
+    type IntoIter = SliceIter<'b, Line<'a>>;
+    fn into_iter(self) -> <Self as std::iter::IntoIterator>::IntoIter {
+        (&self.lines).into_iter()
     }
 }
 
